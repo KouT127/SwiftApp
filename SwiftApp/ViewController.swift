df6d84a1623b8b6722afdecab0c4f9ded270bf37
kou <>
2018-10-23 22:50:21 +0900

    Moyaを導入

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -13,12 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        presentTopView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
+    
+    private func presentTopView() {
+        let storyBoard = getStoryBoard()
+        //withIdentifierに遷移先のstoryboardID
+        let vc = storyBoard.instantiateInitialViewController()!
+        DispatchQueue.main.async {
+            self.present(vc, animated: false, completion: nil)
+        }
+        
+    }
+    
+    private func getStoryBoard() -> UIStoryboard {
+        return UIStoryboard(name: "SampleList", bundle: nil)
+    }
+
 
 
 }
