dc732c339826e9ab387d9295fffc05e615f178a5
kou <>
2018-10-28 19:07:30 +0900

    ファイル階層を変更

---
+++ SwiftApp/GitHub Viewer/GitHub List/SampleRepository.swift
@@ -0,0 +1,13 @@
+//
+//  SampleRepository.swift
+//  SwiftApp
+//
+//  Created by kou on 2018/10/27.
+//  Copyright © 2018 kou. All rights reserved.
+//
+
+import Foundation
+
+class sampleRepository {
+    
+}
