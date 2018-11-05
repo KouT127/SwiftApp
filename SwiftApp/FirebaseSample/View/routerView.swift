aad93b92ef51e9c2c7b3465877f4729febb65236
KouT127 <kou26haru@gmail.com>
2018-11-05 23:00:07 +0900

    初期画面遷移用のクラスを作成

---
+++ SwiftApp/FirebaseSample/View/routerView.swift
@@ -0,0 +1,13 @@
+//
+//  routerView.swift
+//  SwiftApp
+//
+//  Created by kou on 2018/11/05.
+//  Copyright © 2018 kou. All rights reserved.
+//
+
+import Foundation
+
+class routerView {
+    
+}
