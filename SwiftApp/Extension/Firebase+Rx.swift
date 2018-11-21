b0d7edb42e8b3989170dfe0505ddee5f6bde81fe
KouT127 <kou26haru@gmail.com>
2018-11-21 23:21:08 +0900

    Image選択後、FireStorageに上げるところまで実装

---
+++ SwiftApp/Extension/Firebase+Rx.swift
@@ -0,0 +1,135 @@
+//
+//  FirebaseAuth+Rx.swift
+//  SwiftApp
+//
+//  Created by kou on 2018/11/03.
+//  Copyright © 2018 kou. All rights reserved.
+//
+
+import RxSwift
+import FirebaseAuth
+import FirebaseFirestore
+
+extension Reactive where Base: Auth {
+
+    public func signIn(withEmail email: String, password: String) -> Observable<AuthDataResult> {
+        return Observable.create { observer in
+            self.base.signIn(withEmail: email, password: password) { auth, error in
+                if let error = error {
+                    observer.onError(error)
+                } else if let auth = auth {
+                    observer.onNext(auth)
+                    observer.onCompleted()
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    public func createUser(withEmail email: String, password: String) -> Observable<AuthDataResult> {
+        return Observable.create { observer in
+            self.base.createUser(withEmail: email, password: password) { auth, error in
+                if let error = error {
+                    observer.onError(error)
+                } else if let auth = auth {
+                    observer.onNext(auth)
+                    observer.onCompleted()
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
+
+extension Reactive where Base: CollectionReference {
+    public func addDocument(data: [String: Any]) -> Observable<DocumentReference> {
+        return Observable<DocumentReference>.create { observer in
+            var ref: DocumentReference?
+            ref = self.base.addDocument(data: data) { error in
+                if let error = error {
+                    observer.onError(error)
+                } else if let ref = ref {
+                    observer.onNext(ref)
+                }
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+}
+extension Reactive where Base: Query {
+    public func listen() -> Observable<QuerySnapshot> {
+        return Observable<QuerySnapshot>.create { observer in
+            let listener = self.base.addSnapshotListener() { snapshot, error in
+                if let error = error {
+                    observer.onError(error)
+                } else if let snapshot = snapshot {
+                    observer.onNext(snapshot)
+                }
+            }
+            return Disposables.create {
+                listener.remove()
+            }
+        }
+    }
+    public func getDocuments() -> Observable<QuerySnapshot> {
+        return Observable.create { observer in
+            self.base.getDocuments { snapshot, error in
+                if let error = error {
+                    observer.onError(error)
+                } else if let snapshot = snapshot {
+                    observer.onNext(snapshot)
+                    observer.onCompleted()
+                } else {
+                    observer.onError(NSError(domain: FirestoreErrorDomain, code: FirestoreErrorCode.notFound.rawValue, userInfo: nil))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
+
+extension Reactive where Base: DocumentReference {
+    public func setData(_ documentData: [String: Any]) -> Observable<Void> {
+        return Observable<Void>.create { observer in
+            self.base.setData(documentData) { error in
+                guard let error = error else {
+                    observer.onNext(())
+                    observer.onCompleted()
+                    return
+                }
+                observer.onError(error)
+            }
+            return Disposables.create()
+        }
+    }
+    public func getDocument() -> Observable<DocumentSnapshot> {
+        return Observable.create { observer in
+            self.base.getDocument { snapshot, error in
+                if let error = error {
+                    observer.onError(error)
+                } else if let snapshot = snapshot, snapshot.exists {
+                    observer.onNext(snapshot)
+                    observer.onCompleted()
+                } else {
+                    observer.onError(NSError(domain: FirestoreErrorDomain, code: FirestoreErrorCode.notFound.rawValue, userInfo: nil))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    public func listen() -> Observable<DocumentSnapshot> {
+        return Observable<DocumentSnapshot>.create { observer in
+            let listener = self.base.addSnapshotListener() { snapshot, error in
+                if let error = error {
+                    observer.onError(error)
+                } else if let snapshot = snapshot {
+                    observer.onNext(snapshot)
+                }
+            }
+            return Disposables.create {
+                listener.remove()
+            }
+        }
+    }
+}
--- SwiftApp/FirebaseSample/View/FirebaseCreateUserView.swift
+++ SwiftApp/FirebaseSample/View/FirebaseCreateUserView.swift
@@ -11,18 +11,27 @@ import RxSwift
 import RxCocoa
 import FirebaseFirestore
 import FirebaseStorage
+import RxMediaPicker
+import Photos
+import FirebaseStorage
 
-class FirebaseCreateUserView: UIViewController {
+class FirebaseCreateUserView: UIViewController, RxMediaPickerDelegate {
     
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var profile: UITextView!
     @IBOutlet weak var updateButton: UIButton!
+    @IBOutlet weak var userImageButton: RoundButton!
     
+    var picker: RxMediaPicker!
     let db = Firestore.firestore()
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        checkPermission()
+        imagePicker()
 
         let info = Driver.combineLatest(name.rx.text.asDriver(), profile.rx.text.asDriver())
             
@@ -35,6 +44,60 @@ class FirebaseCreateUserView: UIViewController {
                 self.back()
             })
             .disposed(by: disposeBag)
+        
+    }
+    
+    private func imagePicker(){
+        picker = RxMediaPicker(delegate: self)
+        
+        userImageButton.rx.tap
+            .asObservable()
+            .debug()
+            .flatMap {self.pickPhoto()}
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { image in
+                let data = image.1?.jpegData(compressionQuality: 0.8)
+                if let data = data {
+                    Storage.storage().reference(forURL: "").child("images/example.jpg").putData(data)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func present(picker: UIImagePickerController) {
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func dismiss(picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func checkPermission() {
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch photoAuthorizationStatus {
+        case .authorized:
+            print("Access is granted by user")
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({
+                (newStatus) in
+                print("status is \(newStatus)")
+                if newStatus ==  PHAuthorizationStatus.authorized {
+                    /* do stuff here */
+                    print("success")
+                }
+            })
+            print("It is not determined until now")
+        case .restricted:
+            // same same
+            print("User do not have access to photo album.")
+        case .denied:
+            // same same
+            print("User has denied the permission.")
+        }
+    }
+    
+    func pickPhoto() -> Observable<(UIImage, UIImage?)>{
+        return picker.selectImage(editable: true)
     }
     
     private func createUser(name: String?, profile: String?){
