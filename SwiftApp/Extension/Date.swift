e0163a0b3eaba8f187512004d658428ae7606b10
KouT127 <kou26haru@gmail.com>
2018-11-23 11:36:14 +0900

    Extensionを細分化

---
+++ SwiftApp/Extension/Date.swift
@@ -0,0 +1,9 @@
+//
+//  Date.swift
+//  SwiftApp
+//
+//  Created by kou on 2018/11/23.
+//  Copyright © 2018 kou. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftApp/Extension/ObservableType.swift
@@ -0,0 +1,9 @@
+//
+//  ObservableType.swift
+//  SwiftApp
+//
+//  Created by kou on 2018/11/23.
+//  Copyright © 2018 kou. All rights reserved.
+//
+
+import Foundation
